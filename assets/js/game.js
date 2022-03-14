79d19511be3260f210c216d5f4bc5b94815af75d
mrsdno <christine@dno.dev>
2022-03-13 23:31:34 -0400

    added first round of battle

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,9 +1,37 @@
 var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack =10;
 
-// this creates a function named "fight"
+console.log(playerName, playerHealth, playerAttack)
 
-function fight() {
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+
+// alert players they are starting the round
     window.alert("The fight has begun!");
+
+//subtract playerAttack from enemyHealth and use result to update enemyHealth
+
+    enemyHealth = enemyHealth - playerAttack;
+
+//log message in console to confirm it worked
+
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    );
+    
+//subtract enemyAttack from playerHealth and use result to update playerHealth
+
+    playerHealth = playerHealth - enemyAttack;
+
+//log message in console to confirm it worked
+
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+);
 }
 
 fight();
